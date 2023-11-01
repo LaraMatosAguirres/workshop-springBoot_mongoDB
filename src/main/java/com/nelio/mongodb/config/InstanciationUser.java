a6064bbc9e51898999126121a7f39da34047e525
Lara Matos Aguirres <lara.laramatos@gmail.com>
2023-11-01 15:23:09 -0300

    Classe Post

--- src/main/java/com/nelio/mongodb/config/InstanciationUser.java
+++ src/main/java/com/nelio/mongodb/config/InstanciationUser.java
@@ -1,26 +1,42 @@
 package com.nelio.mongodb.config;
 
+import com.nelio.mongodb.domains.Post;
 import com.nelio.mongodb.domains.User;
+import com.nelio.mongodb.repositories.RepositoryPost;
 import com.nelio.mongodb.repositories.RepositoryUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class InstanciationUser implements CommandLineRunner {
 
     @Autowired
     private RepositoryUser repo;
+
+    @Autowired
+    private RepositoryPost repositoryPost;
+
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         repo.deleteAll();
+        repositoryPost.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/08/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"),  "Bom dia", "Acordei feliz hoje!", maria);
+
         repo.saveAll(Arrays.asList(maria, alex, bob));
+        repositoryPost.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/nelio/mongodb/repositories/RepositoryPost.java
@@ -0,0 +1,11 @@
+package com.nelio.mongodb.repositories;
+
+import com.nelio.mongodb.domains.Post;
+import com.nelio.mongodb.domains.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositoryPost extends MongoRepository<Post, String> {
+
+}
